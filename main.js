07e0e49fd475c1413f7a6c1c8e4b97d2ddae6bff
DavidReider <freakdj1997@yahoo.com>
2022-04-27 22:44:02 -0400

    Calculate Tip Amount

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
 (function () {
   console.log("Init");
-  
-  
+  init();
+  function init() {
+    // listen for change on the input
+    document.getElementById("bill").addEventListener("change", function (e) {
+      const fifteen = calculateTip(e.target.value, 15).toFixed(2);
+      const twenty = calculateTip(e.target.value, 20).toFixed(2);
+      const twentyfive = calculateTip(e.target.value, 25).toFixed(2);
+    });
+
+    //listen for click on calculate button
+    document
+      .getElementById("calculate-tip")
+      .addEventListener("click", function (e) {
+        //get custom tip input value
+        const customTip = document.getElementById("custom-tip").value;
+        //get bill input value
+        const bill = document.getElementById("bill").value;
+
+        //calculate tip
+        const tip = calculateTip(bill, customTip).toFixed(2);
+      });
+  }
+
+  function calculateTip(billTotal, tipAmount) {
+    return billTotal * (tipAmount / 100);
+  }
 })();
